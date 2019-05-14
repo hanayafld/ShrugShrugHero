5a16a9c212c34ab66576d0fa9e42402b1d5fd4f2
hanayafld <hanayafld@gmail.com>
2019-05-14 18:02:28 +0900

    Initial commit

---
+++ Dev/Assets/Scripts/Test/TestMetronome.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestYeah : MonoBehaviour
+{
+    float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("메트로놈 테스트");
+        InvokeRepeating("Test", 0, 0.25f);
+    }
+
+    public void TestMetronome(float speed)
+    {
+        speed += 0.25f;
+        Debug.LogFormat("쿵짝 : {0}", this.speed);
+    }
+}
